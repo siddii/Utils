9d8b1f7ec6516ab00b1f6edb2ff2fcf390bf77d9
Siddique Hameed <siddii+git@gmail.com>
2013-07-16 16:33:55 -0500

    updating git-profile.sh

--- git/git-profile.sh
+++ git/git-profile.sh
@@ -1,3 +1,3 @@
-source ~/Scripts/git/git-prompt.sh
-source ~/Scripts/git/git-completion.bash
+source ~/Projects/Utils/git/git-prompt.sh
+source ~/Projects/Utils/git/git-completion.bash
 export PS1='\[\e[0;35m\]\h:\[\e[0;36m\]\w\[\e[0;32m\]$(__git_ps1 " [%s]")\[\e[m\]$ '
