fc95cb7a841c5d7acaa0a4785deea1104a839ab0
Siddique Hameed <siddii+git@gmail.com>
2013-07-17 15:46:00 -0500

    dynamically retrieving script's running directory

--- git/git-profile.sh
+++ git/git-profile.sh
@@ -1,3 +1,4 @@
-source ~/Projects/Utils/git/git-prompt.sh
-source ~/Projects/Utils/git/git-completion.bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $DIR/git-prompt.sh
+source $DIR/git-completion.bash
 export PS1='\[\e[0;35m\]\h:\[\e[0;36m\]\w\[\e[0;32m\]$(__git_ps1 " [%s]")\[\e[m\]$ '
